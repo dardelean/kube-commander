bc4204ebd81dc2f14b0b9b0a8842d144463e387a
dardelean <ardeleandanflorin@gmail.com>
2019-10-10 16:34:27 +0300

    Add NFS SC

--- commander.sh
+++ commander.sh
@@ -12,6 +12,35 @@ kubectl create -n kube-system serviceaccount admin
 kubectl create clusterrolebinding permissive-binding \
 	--clusterrole=cluster-admin --user=admin --user=kubelet --group=system:serviceaccounts
 
+# install helm and addons
+sudo snap install helm
+helm init
+helm repo update
+helm install stable/metrics-server --name metrics-server --namespace kube-system
+### path the metrics server deployment
+#kubectl edit deploy -n kube-system metrics-server
+#        - --kubelet-preferred-address-types=InternalIP
+#        - --kubelet-insecure-tls=true
+#        - --kubelet-port=10250
+
+
+### install NFS SC
+# on comppute node
+sudo apt-get install nfs-common
+
+# on master node
+sudo apt install nfs-kernel-server -y
+sudo mkdir -p /mnt/kubeshare
+sudo chown nobody:nogroup /mnt/kubeshare/
+sudo chmod 777 /mnt/kubeshare/
+# in /etc/exports /mnt/kubeshare 192.168.121.0/24(rw,sync,no_subtree_check)
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
+helm install --name nfs-client-provisioner --namespace kube-system --set nfs.server=192.168.121.10 --set nfs.path=/mnt/kubeshare stable/nfs-client-provisioner
+
+helm install stable/prometheus --name prometheus --namespace kube-system
+
+
 ### useful commands
 #kubectl -n kube-system get cm kubeadm-config -oyaml
 #kubectl describe pod kube-apiserver -n kube-system
